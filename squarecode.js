e0178aebaa2ed88a03050e8fadeda42bb03533c6
Jenny Poon <jenny.poon@live.ca>
2019-01-22 15:55:53 -0800

    solved

--- squarecode.js
+++ squarecode.js
@@ -1,31 +1,34 @@
 function squareCode(input) {
   if (input.length > 81 ) {
-    return "input too long. Max 81 characters"
+    return "input too long. Max 81 characters";
+
   } else {
+
     let arr = []
     let columnLength = Math.ceil(Math.sqrt(input.length))
     let rowLength = Math.floor(Math.sqrt(input.length))
     let encodedMsg = []
 
-
+    //BREAK WORD UP INTO
     while (input.length) {
       arr.push(input.substr(0, columnLength))
       //redefine input
       input = input.substr(columnLength)
     }
+
     console.log("ARR", arr)
-    for (let i = 0; i <= rowLength; i++) {
+
+    for (let i = 0; i <= rowLength ; i++) {
       encodedMsg.push(arr.reduce((accumulator, word) => accumulator + word[i], ""))
     }
-    console.log("ENCODED MSG", encodedMsg)
-    return encodedMsg.join(" ")
 
-// animals.reduce((accumulator, animal) => accumulator + animal[0], '')
+    let squareCode = encodedMsg.join(" ")
+    return squareCode.split(new RegExp('undefined', 'i')).join('');
+
+    // return encodedMsg.join(" ").replace(New ReqExp"undefined", "")
+
   }
 }
 console.log(squareCode("haveaniceday")) //hae and via ecy
 console.log(squareCode("feedthedog"))
 console.log(squareCode("chillout"))
-// have
-// anic
-// eday
